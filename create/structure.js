d3275d5222822272b6897a7a1bef097c0bb1521e
builtbydc <dcarl026@fiu.edu>
2021-10-24 13:11:53 -0400

    change style, add song

--- create/structure.js
+++ create/structure.js
@@ -1,4 +1,7 @@
 const pageTitle = "Example Page";
+const songURL = "https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/" +
+    "1147143079%3Fsecret_token%3Ds-ONkbQu9GkHs&color=%23c792df&auto_play=false&hide_related=false&" +
+    "show_comments=false&show_user=true&show_reposts=false&show_teaser=false&visual=false"
 
 function loadPage() {
     document.getElementById("page-title").innerHTML = pageTitle;
@@ -8,6 +11,7 @@ function loadPage() {
 function Structure() {
     return build([
         Div("", "", "background"),
-        Header(H(1, headerText(), "NO_DIV", "header-text"))
+        Header(H(1, headerText(), "NO_DIV", "header-text")),
+        '<iframe id="aliens3-frame" width="100%" height="20" scrolling="no" frameborder="no" allow="autoplay" src=' + ttL(songURL) + '></iframe>',
     ]);
 }
