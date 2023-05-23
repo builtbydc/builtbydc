ed52573a207a4e6f857b79bdc7f756e2a22af6a3
builtbydc <dcarl026@fiu.edu>
2023-05-23 13:33:31 -0600

    cool arrangement

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,12 +1,12 @@
 let particles = [];
-let numParticles;
+let particleNumber = 500;
 
 let colors = [];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    numParticles = ~~(windowWidth * windowHeight / 500);
-    console.log(numParticles);
+
+    noStroke();
 
     colors.push(color(255, 173, 173));
     colors.push(color(255, 214, 165));
@@ -17,68 +17,80 @@ function setup() {
     colors.push(color(189, 178, 255));
     colors.push(color(255, 198, 255));
     colors.push(color(255, 255, 252));
+
+    let particleSize = 5;
+    for(let i = 0; i < particleNumber; i++) {
+        let px = windowWidth*Math.random();
+        let py = windowHeight*Math.random();
+        particles[i] = new Particle(px, py, particleSize, colors[~~(9*Math.random())], i);
+    }
+
+    background(0);
 }
 
 function draw() {
-    background(0);
-    if (particles.length < numParticles) {
-        let x = width * Math.random();
-        let y = height * Math.random() - height;
-        let r = 3 * (y + height) / height + 3;
-        let c = colors[~~(9*Math.random())];
-        particles.push(new Particle(x, y, r, c));
-    }
-    for (let i = 0; i < particles.length; i++) {
-        let p = particles[i];
-        if (p.y > windowHeight) {
-            let x = width * Math.random();
-            let y = height * Math.random() - height;
-            let r = 3 * (y + height) / height + 3;
-            let c = colors[~~(9*Math.random())];
-            particles[i] = new Particle(x, y, r, c);
-        }
+    //background(0);
+
+    for(let p of particles) {
         p.display();
     }
 }
 
 class Particle {
-    constructor(x, y, r, c) {
+    constructor(x, y, size, color, index) {
         this.x = x;
-        this.y = y;
-        this.r = r;
-
-        this.c = c;
-
-        this.dx = 0;
+        this.y = y; 
+        this.size = size;
+        this.color = color;
+        this.index = index;
+        
+        this.dx = 0; 
         this.dy = 0;
     }
 
-    update() {
-        if(mouseIsPressed) {
-            let tempx = Math.abs(this.x - mouseX);
-            let tempy = Math.abs(this.y - mouseY);
-            let tempd = tempx * tempx + tempy * tempy;
-            let theta = Math.atan(tempy / tempx);
-
-            if (this.x < mouseX) this.dx -= Math.min(5, 5000*Math.cos(theta) / tempd);
-            else this.dx += Math.min(5, 5000 * Math.cos(theta) / tempd);
-
-            if(this.y < mouseY) this.dy -= Math.min(5, 5000 * Math.sin(theta) / tempd);
-            else this.dy += Math.min(5, 5000 * Math.sin(theta) / tempd);
-        }
-
-        if(this.dy < 0) this.dy *= 0.95;
-        this.dx *= 0.95;
-
-        this.dy += 0.05;
-        this.x += this.dx;
-        this.y += this.dy;
+    display() {
+        fill(this.color);
+        ellipse(this.x, this.y, this.size, this.size);
+        this.update();
     }
 
-    display() {
-        this.update();
-        noStroke();
-        fill(this.c);
-        ellipse(this.x, this.y, this.r, this.r);
+    update() {
+        for(let i = 0; i < particleNumber; i++) {
+            if(i !== this.index) {
+                let other = particles[i];
+                let xdist = other.x - this.x;
+                let ydist = other.y - this.y;
+                let dist = Math.sqrt(xdist*xdist + ydist*ydist);
+                let theta = Math.atan2(ydist, xdist);
+
+                if(Math.abs(dist) > 1) {
+                    let forceConstant = 7;
+                    this.dx -= forceConstant * Math.cos(theta) / (dist*dist);
+                    this.dy -= forceConstant * Math.sin(theta) / (dist*dist);
+                }
+            }
+        }
+        this.x += this.dx;
+        this.y += this.dy;
+
+        if(this.x < this.size/2) {
+            this.dx *= -0.5;
+            this.x = this.size/2;
+        }
+        if(this.x > windowWidth - this.size/2) {
+            this.dx *= -0.5;
+            this.x = windowWidth - this.size/2;
+        }
+        if(this.y < this.size/2) {
+            this.dy *= -0.5;
+            this.y = this.size/2;
+        }
+        if(this.y > windowHeight-this.size/2) {
+            this.dy *= -0.5;
+            this.y = windowHeight - this.size/2;
+        }
+
+        this.dx *= 0.99;
+        this.dy *= 0.99;
     }
 }
