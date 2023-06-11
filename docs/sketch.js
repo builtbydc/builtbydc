9f9ca0f370a3a96ea421d6b286c0739cfd487d99
builtbydc <dcarl026@fiu.edu>
2023-06-10 18:15:26 -0600

    restart

--- docs/sketch.js
+++ docs/sketch.js
@@ -18,79 +18,51 @@ function setup() {
     colors.push(color(255, 198, 255));
     colors.push(color(255, 255, 252));
 
-    let particleSize = 5;
-    for(let i = 0; i < particleNumber; i++) {
-        let px = windowWidth*Math.random();
-        let py = windowHeight*Math.random();
-        particles[i] = new Particle(px, py, particleSize, colors[~~(9*Math.random())], i);
-    }
-
     background(0);
+    
+
+    
 }
+
+let x = 0;
+let y = 0;
 
 function draw() {
-    //background(0);
+    for(let i = 0; i < 10000; i++) {
+        push();
+        translate(windowWidth / 2, windowHeight);
 
-    for(let p of particles) {
-        p.display();
+        px = x * windowHeight / 15;
+        py = -y * windowHeight / 12;
+
+        if(py <= 0 && -py < windowHeight) {
+            fill(colors[~~(-9*py / windowHeight)]);
+            ellipse(px, py, 1, 1);
+        }
+
+        pop();
+
+        let nextX;
+        let nextY;
+
+        let r = Math.random();
+
+        if(r < 0.01) {
+            nextX = 0;
+            nextY = 0.001 * y;
+        } else if(r < 0.86) {
+            nextX = 1.3 * x;
+            nextY = 0.84 * y + 1.6;
+        } else if(r < 0.93) {
+            nextX = 0.1 * x - 0.2 * y;
+            nextY = 0.3 * x + 0.05 * y;
+        } else {
+            nextX = -0.1 * x + 0.2 * y;
+            nextY = 0.3 * x + 0.05 * y;
+        }
+
+        x = nextX;
+        y = nextY;
     }
 }
 
-class Particle {
-    constructor(x, y, size, color, index) {
-        this.x = x;
-        this.y = y; 
-        this.size = size;
-        this.color = color;
-        this.index = index;
-        
-        this.dx = 0; 
-        this.dy = 0;
-    }
-
-    display() {
-        fill(this.color);
-        ellipse(this.x, this.y, this.size, this.size);
-        this.update();
-    }
-
-    update() {
-        for(let i = 0; i < particleNumber; i++) {
-            if(i !== this.index) {
-                let other = particles[i];
-                let xdist = other.x - this.x;
-                let ydist = other.y - this.y;
-                let dist = Math.sqrt(xdist*xdist + ydist*ydist);
-                let theta = Math.atan2(ydist, xdist);
-
-                if(Math.abs(dist) > 1) {
-                    let forceConstant = 7;
-                    this.dx -= forceConstant * Math.cos(theta) / (dist*dist);
-                    this.dy -= forceConstant * Math.sin(theta) / (dist*dist);
-                }
-            }
-        }
-        this.x += this.dx;
-        this.y += this.dy;
-
-        if(this.x < this.size/2) {
-            this.dx *= -0.5;
-            this.x = this.size/2;
-        }
-        if(this.x > windowWidth - this.size/2) {
-            this.dx *= -0.5;
-            this.x = windowWidth - this.size/2;
-        }
-        if(this.y < this.size/2) {
-            this.dy *= -0.5;
-            this.y = this.size/2;
-        }
-        if(this.y > windowHeight-this.size/2) {
-            this.dy *= -0.5;
-            this.y = windowHeight - this.size/2;
-        }
-
-        this.dx *= 0.99;
-        this.dy *= 0.99;
-    }
-}
